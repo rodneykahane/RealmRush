983e7cbf61fbbf546247785532642713d9bac110
rodney <rodney@rodneykahan.es>
2019-09-10 16:49:03 -0400

    implementing pathfinding

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -27,9 +27,9 @@ public class CubeEditor : MonoBehaviour
     {
         int gridSize = waypoint.GetGridSize();
         transform.position = new Vector3(
-            waypoint.GetGridPos().x,
+            waypoint.GetGridPos().x * gridSize,
             0f,
-            waypoint.GetGridPos().y);
+            waypoint.GetGridPos().y * gridSize);
     }
 
     private void UpdateLabel()
@@ -38,9 +38,9 @@ public class CubeEditor : MonoBehaviour
 
         TextMesh textMesh = GetComponentInChildren<TextMesh>();
         string labelText = 
-            waypoint.GetGridPos().x / gridSize + 
+            waypoint.GetGridPos().x  + 
             ", " + 
-            waypoint.GetGridPos().y / gridSize;
+            waypoint.GetGridPos().y;
         textMesh.text = labelText;
         gameObject.name = labelText;
     }
--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -7,12 +7,83 @@ public class Pathfinder : MonoBehaviour
 {
     [SerializeField] Waypoint startWaypoint, endWaypoint;
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true;
+
+
+    Vector2Int[] directions = {
+
+       Vector2Int.up,
+       Vector2Int.right,
+       Vector2Int.down,
+       Vector2Int.left
+       
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        Pathfind();
+        //ExploreNeighors();
+    }
+
+    private void Pathfind()
+    {
+
+        queue.Enqueue(startWaypoint);
+
+        while (queue.Count > 0 && isRunning)
+        {
+            var searchCenter = queue.Dequeue();
+            print("Searching from: " + searchCenter); // todo remove later
+            HaltIfEndFound(searchCenter);
+            ExploreNeighors(searchCenter);
+        }
+
+        print("finished pathfinding?");
+        
+    }
+
+    private void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            Debug.Log("Searching from end node, quitting");
+            isRunning = false;            
+        }
+        else
+        {
+
+            //print("doing stuff");
+        }
+    }
+
+    private void ExploreNeighors(Waypoint from)
+    {
+        if (!isRunning)
+        {
+            return;  //exit if isRunning is false
+        }
+        foreach(Vector2Int direction in directions)
+        {           
+            Vector2Int neighborCoordinates = startWaypoint.GetGridPos() + direction;
+            
+            try
+            {
+                Waypoint neighbor = grid[neighborCoordinates];
+                neighbor.SetTopColor(Color.blue);
+                queue.Enqueue(neighbor);
+                print("Queuing " + neighbor);
+            }
+            catch
+            {
+                // do nothing
+                //Debug.Log("non existent waypoint");
+            }
+
+        }
     }
 
     private void ColorStartAndEnd()
