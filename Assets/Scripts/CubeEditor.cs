ada531a7ef5b927d31acd5f1cb9e7e1aacf7768b
rodney <rodney@rodneykahan.es>
2019-09-10 08:54:40 -0400

    sourcetree test

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour
+{   
+    Waypoint waypoint;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x,
+            0f,
+            waypoint.GetGridPos().y);
+    }
+
+    private void UpdateLabel()
+    {
+        int gridSize = waypoint.GetGridSize();
+
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = 
+            waypoint.GetGridPos().x / gridSize + 
+            ", " + 
+            waypoint.GetGridPos().y / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+    //int cnt = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintAllWayPoints());
+        print("back at start");
+    }
+
+    IEnumerator PrintAllWayPoints()
+    {
+        print("Starting patrol ...");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting " + waypoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol ...");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*  my challenge solution
+     
+    IEnumerator PrintAllWayPoints()
+    {
+        foreach (Waypoint waypoint in path)
+        {          
+            if(cnt == 1)
+            {
+                //print("starting patrol (block: " + (transform.position.x / 10) + ", " + (transform.position.z / 10) + "), cnt is: " + cnt +", path.Count is: "+path.Count);
+                print("Starting patrol!");
+                cnt++;
+                transform.position = waypoint.transform.position;
+            }
+            else if(cnt > 0 && cnt < path.Count)
+            {
+                transform.position = waypoint.transform.position;
+                //print("visiting block: " + (transform.position.x / 10) + ", " + (transform.position.z / 10)+ ", cnt is: " + cnt);
+                print("visiting block: " + (transform.position.x / 10) + ", " + (transform.position.z / 10));
+                cnt++;
+            }
+            else
+            {
+                transform.position = waypoint.transform.position;
+                //print("ending patrol (block: " + (transform.position.x / 10) + ", " + (transform.position.z / 10) + "), cnt is: " + cnt);
+                print("Ending patrol!");
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+
+
+    }
+
+
+
+     */
+
+}
